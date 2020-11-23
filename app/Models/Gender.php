4f0652a30a363d453a3ed52983cd30e33b350749
Clayton Figueiredo <claytongf@gmail.com>
2020-11-23 13:43:13 -0300

    CRUD Categories and Genders

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Gender extends Model
 {
     use HasFactory, SoftDeletes, Uuid;
 
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
         'id' => 'string'
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\GenderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
+Route::resource('genders', GenderController::class)->except(['create', 'edit']);
